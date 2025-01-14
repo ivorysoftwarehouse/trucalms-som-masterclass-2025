9ba2081b54dfd6eae33aa5d5790ffbff03cecfb7
Precious Uwhubetine <preciousbetine@gmail.com>
2025-01-14 13:59:32 +0100

    Update #1

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,7 @@
 function toTitleCase(str) {
+  if (!str || str.length === 0) {
+    return str;
+  }
   return str.replace(
     /\w\S*/g,
     text => text.charAt(0).toUpperCase() + text.substring(1).toLowerCase()
